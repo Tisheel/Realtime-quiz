e89adca7f831acda0c20bfc764b646f67e1be698
Tisheel Bashyam <tisheelbashyam@gmail.com>
2024-03-24 14:39:43 +0530

    https/wss

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,4 @@
-export const WS_ROOM = `ws://${window.location.host}/room`
-export const WS_TEACHER = `ws://${window.location.host}/teacher`
-export const GET_TESTS = `http://${window.location.host}/v1/test`
-export const LOGIN = `http://${window.location.host}/v1/login`
+export const WS_ROOM = `wss://${window.location.host}/room`
+export const WS_TEACHER = `wss://${window.location.host}/teacher`
+export const GET_TESTS = `https://${window.location.host}/v1/test`
+export const LOGIN = `https://${window.location.host}/v1/login`
